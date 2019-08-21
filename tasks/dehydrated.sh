2b8703c39cc9973940cadd62f0dec67a480f86b7
Robert Van Voorhees <rcvanvo@gmail.com>
2019-08-21 14:17:18 -0400

    Refactor into proper scripts.

---
+++ tasks/dehydrated.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eux
+
+ls -la ${PWD}/certificates/
+ls -la ${PWD}/certificates-version/
+mkdir -p ${PWD}/certificates/ssl/
+
+echo "${ENV_NAME}.${DNS_SUFFIX} *.${ENV_NAME}.${DNS_SUFFIX} *.pks.${ENV_NAME}.${DNS_SUFFIX} > private" > ${PWD}/domains.txt
+
+cat ${PWD}/domains.txt
+
+echo "DOMAINS_TXT=${PWD}/domains.txt
+CA=${CA}
+CURL_OPTS='${CURL_OPTS}'
+CHALLENGETYPE=dns-01
+HOOK=/home/dehydrated/hook.sh
+HOOK_CHAIN=no
+CHAINCACHE=${PWD}/certificates/ssl/chains
+CERTDIR=${PWD}/certificates/ssl
+ACCOUNTDIR=${PWD}/certificates/ssl/accounts" >> ${PWD}/config
+
+cat ${PWD}/config
+
+/home/dehydrated/dehydrated --accept-terms -c -n -f ${PWD}/config
+
+mkdir -p ${PWD}/updated-certificates
+tar cvfz ${PWD}/updated-certificates/certificates-$(cat certificates-version/version).tgz -C ${PWD}/certificates/ ssl/
+ls -lah ${PWD}/certificates/ ${PWD}/updated-certificates

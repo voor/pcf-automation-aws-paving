ed4891b31778f79f20396e04d6a965d26958a4c9
Robert Van Voorhees <rcvanvo@gmail.com>
2019-08-26 12:08:18 -0400

    Change this to just refer directly avoiding ordering issues.

--- terraform_override_files/harbor.tf
+++ terraform_override_files/harbor.tf
@@ -218,11 +218,7 @@ resource "aws_iam_policy" "ops_manager_harbor_policy" {
   policy      = "${data.aws_iam_policy_document.ops_manager_harbor.json}"
 }
 
-data "aws_iam_role" "data_ops_manager_role" {
-  name = "${var.env_name}_om_role"
-}
-
 resource "aws_iam_role_policy_attachment" "ops_manager_policy" {
-  role       = "${data.aws_iam_role.data_ops_manager_role.name}"
+  role       = "${var.env_name}_om_role"
   policy_arn = "${aws_iam_policy.ops_manager_harbor_policy.arn}"
 }

dedfc4a77457246176a0f9eeaa0a3a2fab05178d
Robert Van Voorhees <rcvanvo@gmail.com>
2019-10-08 12:34:50 -0400

    System ingress (#3)
    * Change ordering to deal with failure better.
    
    * Expose certificates to ingress cluster so it can use them.
    
    * Different function name than helm.
    
    * Still testing, but pretty sure this is the right path....
    
    * Fixing folder overwrite issue.
    
    * Adds in more manifests (default storage class) and makes the cluster task a little more generic.
    
    * Bug fix.
    
    * Not sure what was going on here...
    
    * Ah, I get it, I was halfway through changing this.
    
    * Pretty output.
    
    * Oops, say it two places.
    
    * This is called weekend programming.

---
+++ tasks/create-cluster.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+set -e
+chmod 600 env/"${OPS_MANAGER_KEY_PATH}"
+eval "$(om --env env/"${ENV_FILE}" bosh-env --ssh-private-key env/"${OPS_MANAGER_KEY_PATH}")"
+
+export PKS_PASSWORD=$(om --env env/"${ENV_FILE}" credentials --product-name pivotal-container-service -c .properties.uaa_admin_password -f secret)
+export PKS_USERNAME="admin"
+export PKS_ENDPOINT="https://api.pks.${ENV_NAME}.${DNS_NAME}"
+export PKS_CLI=$(ls -1 pks-cli/pks-linux-amd64-*)
+
+chmod +x $PKS_CLI
+$PKS_CLI login -a ${PKS_ENDPOINT} -u ${PKS_USERNAME} -p ${PKS_PASSWORD} --ca-cert /etc/ssl/certs/ca-certificates.crt
+
+set -ux
+set +e
+
+$PKS_CLI cluster ${CLUSTER_NAME}
+
+if [ $? -ne 0 ]; then
+    echo "Creating ${CLUSTER_NAME} cluster..."
+    $PKS_CLI create-cluster ${CLUSTER_NAME} -n 2 -p ${PLAN_NAME} -e ${CLUSTER_NAME}.pks.${ENV_NAME}.${DNS_NAME}
+else
+    echo "${CLUSTER_NAME} cluster already exists, checking it's created, retrieving information and making sure subnet tags are there..."
+fi
+
+while $PKS_CLI cluster ${CLUSTER_NAME} | grep -m 1 "In Progress"; do sleep 15; done
+
+set -e
+
+mkdir -p master-instances
+
+echo "cluster_name: ${CLUSTER_NAME}" > master-instances/${CLUSTER_NAME}.cluster.yml
+echo "cluster_host: ${CLUSTER_NAME}.pks.${ENV_NAME}.${DNS_NAME}" >> master-instances/${CLUSTER_NAME}.cluster.yml
+
+$PKS_CLI cluster ${CLUSTER_NAME} --json >> master-instances/${CLUSTER_NAME}.pks-cluster.json
+bosh vms
+bosh vms --json >> master-instances/${CLUSTER_NAME}.bosh-vms.json
+
+export CLUSTER_UUID=$($PKS_CLI cluster ${CLUSTER_NAME} --json | om interpolate --path /uuid)
+aws ec2 create-tags --resources $(cat env/${SUBNETS_FILE}) --tags Key=kubernetes.io/cluster/service-instance_${CLUSTER_UUID},Value=shared
+
+export KUBECONFIG=master-instances/${CLUSTER_NAME}.kubeconfig
+
+$PKS_CLI get-credentials ${CLUSTER_NAME}
+
+echo "Output kubeconfig for cluster ${CLUSTER_NAME} to ${KUBECONFIG}"

a6eeb411d47280a33a825d4d58b0549b28f76b7f
Robert Van Voorhees <rcvanvo@gmail.com>
2019-10-11 14:01:22 -0400

    Allow passing without terraform output.

--- tasks/interpolate.sh
+++ tasks/interpolate.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env sh
 
+
+# Pass in the terraform output as a variable, so if the file doesn't exist the interpolate won't fail.
+TERRAFORM_OUTPUT=""
+if [[ -f "$PWD/terraform-output/metadata" ]]; then
+    TERRAFORM_OUTPUT="-d data=file://$(pwd)/terraform-output/metadata?type=application/json"
+fi
+
 set -eu
 mkdir -p interpolated-config/$ENV_NAME/config
 /bin/gomplate --input-dir config/$ENV_NAME/config \
-    -d data=file://$(pwd)/terraform-output/metadata?type=application/json \
+    $TERRAFORM_OUTPUT \
     -d fullchain=file://$(pwd)/certificates/ssl/private/fullchain.pem \
     -d privkey=file://$(pwd)/certificates/ssl/private/privkey.pem \
     --output-dir interpolated-config/$ENV_NAME/config --verbose
 
 mkdir -p interpolated-config/$ENV_NAME/env
 /bin/gomplate -f config/$ENV_NAME/env/env.yml \
-    -d data=file://$(pwd)/terraform-output/metadata?type=application/json \
+    $TERRAFORM_OUTPUT \
     -o interpolated-config/$ENV_NAME/env/env.yml --verbose
 
 mkdir -p interpolated-config/$ENV_NAME/download-product-configs
 /bin/gomplate --input-dir config/$ENV_NAME/download-product-configs \
-    -d data=file://$(pwd)/terraform-output/metadata?type=application/json \
+    $TERRAFORM_OUTPUT \
     --output-dir interpolated-config/$ENV_NAME/download-product-configs --verbose

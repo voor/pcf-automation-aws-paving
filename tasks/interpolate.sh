1cb9b0c77096ea5b6319e6ca21f9de12775b7997
Robert Van Voorhees <rcvanvo@gmail.com>
2019-10-11 14:08:07 -0400

    Allow interpolate without terraform output.

--- tasks/interpolate.sh
+++ tasks/interpolate.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env sh
 
 
+set -eu
+
 # Pass in the terraform output as a variable, so if the file doesn't exist the interpolate won't fail.
 TERRAFORM_OUTPUT=""
 if [[ -f "$PWD/terraform-output/metadata" ]]; then
-    TERRAFORM_OUTPUT="-d data=file://$(pwd)/terraform-output/metadata?type=application/json"
-fi
+  TERRAFORM_OUTPUT="-d data=file://$(pwd)/terraform-output/metadata?type=application/json"
 
-set -eu
-mkdir -p interpolated-config/$ENV_NAME/config
-/bin/gomplate --input-dir config/$ENV_NAME/config \
+  mkdir -p interpolated-config/$ENV_NAME/config
+  /bin/gomplate --input-dir config/$ENV_NAME/config \
     $TERRAFORM_OUTPUT \
     -d fullchain=file://$(pwd)/certificates/ssl/private/fullchain.pem \
     -d privkey=file://$(pwd)/certificates/ssl/private/privkey.pem \
     --output-dir interpolated-config/$ENV_NAME/config --verbose
 
-mkdir -p interpolated-config/$ENV_NAME/env
-/bin/gomplate -f config/$ENV_NAME/env/env.yml \
+  mkdir -p interpolated-config/$ENV_NAME/env
+  /bin/gomplate -f config/$ENV_NAME/env/env.yml \
     $TERRAFORM_OUTPUT \
     -o interpolated-config/$ENV_NAME/env/env.yml --verbose
 
+fi
+
+# The product configs do not need terraform to work, so we run them separately.
 mkdir -p interpolated-config/$ENV_NAME/download-product-configs
 /bin/gomplate --input-dir config/$ENV_NAME/download-product-configs \
-    $TERRAFORM_OUTPUT \
-    --output-dir interpolated-config/$ENV_NAME/download-product-configs --verbose
+  --output-dir interpolated-config/$ENV_NAME/download-product-configs --verbose
